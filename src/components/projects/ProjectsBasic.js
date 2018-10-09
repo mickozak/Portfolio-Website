2452f79aa26f20acb92af67545d2ad9bcb8d621d
MichałKozak <kozak_m@outlook.com>
2018-10-09 21:17:14 +0200

    new project in html/css

--- src/components/projects/ProjectsBasic.js
+++ src/components/projects/ProjectsBasic.js
@@ -8,20 +8,20 @@ class ProjectsBasic extends React.Component{
     render(){
         return(
             <Grid container spacing={5}>
-                <Grid item xs container justify="center" style={{padding:'0.5em'}}>
+                <Grid item xs container justify="center" style={{padding:'1em'}}>
                     <Card
                         img={"http://s6.ifotos.pl/img/htmlcssjp_qeqwqsr.jpg"}
                         projectName={"Active Student"}
-                        projectDescription={" Website prepared for the student organization."}
+                        projectDescription={"Website prepared for the student organization."}
                         github={"https://github.com/mickozak/Website-ActiveStudent"}
                         livedemo={"https://mickozak.github.io/Website-ActiveStudent/"}
                     />
                 </Grid>
-                <Grid item xs container justify="center" style={{padding:'0.5em'}}>
+                <Grid item xs container justify="center" style={{padding:'1em'}}>
                     <Card
                         img={"http://s6.ifotos.pl/img/htmlcssjp_qeqwqsr.jpg"}
                         projectName={"CSS Effect"}
-                        projectDescription={"The repository contains various mini CSS projects."}
+                        projectDescription={"The repository contains various mini projects."}
                         github={"https://github.com/mickozak/CSS"}
                         livedemo={"https://mickozak.github.io/CSS/"}
                     />
