f8e95ec4ca472c6ca2341c34363a0a4231b9879e
M. Kozak <kozak_m@outlook.com>
2018-10-13 21:36:06 +0200

    Game Mole & Game Car

--- src/components/projects/ProjectsJavaScript.js
+++ src/components/projects/ProjectsJavaScript.js
@@ -13,18 +13,18 @@ class ProjectsReact extends React.Component{
                     <Card
                         img={"https://miro.medium.com/max/1108/1*H-25KB7EbSHjv70HXrdl6w.png"}
                         projectName={"Game Mole"}
-                        projectDescription={"In progrss..."}
-                        github={"#"}
-                        livedemo={"#"}
+                        projectDescription={"A simple game in which our goal is to kill the mole."}
+                        github={"https://github.com/mickozak/Game-Mole"}
+                        livedemo={"https://mickozak.github.io/Game-Mole/"}
                     />
                 </Grid>
                 <Grid item xs container justify="center" style={{padding:'0.5em'}}>
                     <Card
                         img={"https://miro.medium.com/max/1108/1*H-25KB7EbSHjv70HXrdl6w.png"}
                         projectName={"Game Cars"}
-                        projectDescription={"In progress..."}
-                        github={"#"}
-                        livedemo={"#"}
+                        projectDescription={"A simple game where you must collect points and avoid bombs."}
+                        github={"https://github.com/mickozak/Game-Car"}
+                        livedemo={"https://mickozak.github.io/Game-Car/"}
                     />
                 </Grid>
                 <Grid item xs container justify="center" style={{padding:'0.5em'}}>
